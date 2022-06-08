8754b1de39b181098543133f09a4d1b23ef36897
giovane <giovane_05silva@hotmail.com>
2022-06-08 11:19:50 -0300

    declarando variáveis

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const question       = document.querySelector("#question");
+const answersBox     = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
